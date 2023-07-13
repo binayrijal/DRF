0d4f44532477c723b6383475c31fd09284d15f35
binayrijal <binayrijal22@gmail.com>
2023-07-13 20:12:32 +0545

    pk value in get url

--- crudoperation/apiview/viewstart/views.py
+++ crudoperation/apiview/viewstart/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializer import StudentSerializer
+from .models import Student
 
 
 
 # Create your views here.
 @api_view(['GET','POST','PUT','PATCH','DELETE'])
-def helloworld(request):
+def helloworld(request,pk=None):
   if request.method=="GET":
-    return Response({'msg':'hello world'})
+    id=pk
+    if id is not None:
+     stu=Student.objects.get(id=id)
+     serializer=StudentSerializer(stu)
+     return Response(serializer.data)
+    stu=Student.objects.all()
+    serializer=StudentSerializer(stu,many=True)
+    return Response(serializer.data)
 
   if request.method=="POST": 
     print(request.data)
