dfe224edfe50ac9e7958348cfe84794d57dfe891
binayrijal <binayrijal22@gmail.com>
2023-07-15 18:45:43 +0545

    put and patch added in views

--- ClassApiview/classapi/classApiStart/views.py
+++ ClassApiview/classapi/classApiStart/views.py
@@ -5,7 +5,8 @@ from rest_framework.response import Response
 
 
 class StudentAPI(APIView):
-    def get(self,request,format=None,pk=None):
+    
+    def get(self,request,pk=None,format=None):
         id=pk
         if id is not None:
          stu=Student.objects.get(id=id)
@@ -14,7 +15,7 @@ class StudentAPI(APIView):
         stu=Student.objects.all()
         serializer=StudentSerializer(stu,many=True)
         return Response(serializer.data)
-    def post(self,request,format=None,):
+    def post(self,request,format=None):
        data=request.data
        print(data)
        serializer=StudentSerializer(data=data)
@@ -22,4 +23,22 @@ class StudentAPI(APIView):
           serializer.save()
           return Response({'msg':'data created'})
        
-
+    def patch(self,request,pk,format=None):
+       id=pk
+       data=request.data
+       stu=Student.objects.get(id=id)
+       serializer=StudentSerializer(stu,partial=True,data=data)
+       if serializer.is_valid():
+         serializer.save()
+         return Response({'msg':'data updated partilly'})
+    
+       return Response(serializer.errors)
+    def put(self,request,pk,format=None):
+       id=pk
+       data=request.data
+       stu=Student.objects.get(id=id)
+       serializer=StudentSerializer(stu,data=data)
+       if serializer.is_valid():
+          serializer.save()
+          return Response({'msg':'data update complete'})
+       return Response(serializer.errors)
