3f2749b06a1b13fa955a606eff09a010d12c8d99
binayrijal <binayrijal22@gmail.com>
2023-07-16 14:46:27 +0545

    alternate method added to views.py for crud operations

--- crudoperation/genericapi/genericstart/urls.py
+++ crudoperation/genericapi/genericstart/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     path('student/',views.StudentGETPOSTAPI.as_view(),name="student"),
-    path('student/<int:pk>/',views. StudentUPDATEDELETE.as_view(),name="student")
+    path('student/<int:pk>/',views. StudentUPDATEDELETE.as_view(),name="student"),
+    path('apistudent/',views.StudentGETPOSTAPI.as_view()),
+    path('apistudent/<int:pk>/',views. StudentUPDATEDELETE.as_view(),name="student"),
 ]
--- crudoperation/genericapi/genericstart/views.py
+++ crudoperation/genericapi/genericstart/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Student
 from .serializer import StudentSerializer
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView,ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
 
 # Create your views here.
@@ -23,4 +23,19 @@ class StudentUPDATEDELETE(GenericAPIView,RetrieveModelMixin,UpdateModelMixin,Des
         return self.update(request, *args, **kwargs)
     def delete(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs) 
+    
+
+#alternate: implement crud operation just in two function with in max 5 line of code 
+
+
+class StudentGETPOSTAPI(ListCreateAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+
+
+class StudentUPDATEDELETE(RetrieveUpdateDestroyAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
 
