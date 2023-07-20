22ad919e7f09313ca4ea29f53e06cdf03e93e29e
binayrijal <binayrijal22@gmail.com>
2023-07-20 19:00:10 +0545

    authentication and permission inside class

--- crudoperation/modelviewset/modelviewstart/views.py
+++ crudoperation/modelviewset/modelviewstart/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from .models import Student
 from rest_framework import viewsets
 from .serializer import StudentSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
-    
+    authentication_classes=[BasicAuthentication]
+    permission_classes=[IsAuthenticated]
+
 class StudentReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
