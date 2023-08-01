1b5fa5a3d8ed78bf9d89aad3e5e5d9e59548c2ba
binayrijal <binayrijal22@gmail.com>
2023-08-01 15:08:43 +0545

    added Djangomodelviewpermission class

--- crudoperation/modelviewset/modelviewset/urls.py
+++ crudoperation/modelviewset/modelviewset/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('modelviewstart.urls'))
+    path('',include('modelviewstart.urls')),
+    path('auth/',include('rest_framework.urls'))
 ]
 
--- crudoperation/modelviewset/modelviewstart/views.py
+++ crudoperation/modelviewset/modelviewstart/views.py
@@ -3,7 +3,7 @@ from .models import Student
 from rest_framework import viewsets
 from .serializer import StudentSerializer
 from rest_framework.authentication import BasicAuthentication,SessionAuthentication
-from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions
 
 # Create your views here.
 class StudentModelViewSet(viewsets.ModelViewSet):
@@ -23,8 +23,9 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     #this is session authentication
     authentication_classes=[SessionAuthentication]
     #permission_classes=[IsAuthenticated]
-    permission_classes=[IsAdminUser]
+    #permission_classes=[IsAdminUser]
     #permission_classes=[IsAuthenticatedOrReadOnly]
+    permission_classes=[DjangoModelPermissions]
 
 class StudentReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
     queryset=Student.objects.all()
