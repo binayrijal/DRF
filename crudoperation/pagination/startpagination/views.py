b16ba80286442caf72d6e04d6591f2d09ee98752
binayrijal <binayrijal22@gmail.com>
2023-08-05 22:04:41 +0545

    pagination

---
+++ crudoperation/pagination/startpagination/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .serializer import StudentSerializer
+from .models import Student
+from .mypagination import MyPageNumberPagination
+
+
+
+# Create your views here.
+class StudentList(ListAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+    pagination_class=[MyPageNumberPagination]
+    
