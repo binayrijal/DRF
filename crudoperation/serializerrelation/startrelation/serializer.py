e61a1f389b1437aa0d32ab1605dbcc4833aecad8
binayrijal <binayrijal22@gmail.com>
2023-08-06 18:12:01 +0545

    add extra attribute in singer serializer

--- crudoperation/serializerrelation/startrelation/serializer.py
+++ crudoperation/serializerrelation/startrelation/serializer.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import Singer,Song
 
 class SingerSerializer(serializers.ModelSerializer):
-    songs=serializers.StringRelatedField(many=True,read_only=True)
+    #songs=serializers.StringRelatedField(many=True,read_only=True)
+    #songs=serializers.PrimaryKeyRelatedField(many=True,read_only=True)
+    songs=serializers.HyperlinkedIdentityField(many=True,read_only=True,view_name='song-detail')
     class Meta:
         model=Singer
         fields='__all__'
